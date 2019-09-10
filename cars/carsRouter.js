5a6039757c04d4998edd37877c9af4f1ab0c9425
Faye Skeen <fskeen@gmail.com>
2019-09-10 16:07:06 -0700

    Get by ID and POST are working.

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -10,6 +10,40 @@ router.get('/', (req, res) => {
         .then(car => {
             res.status(200).json(car)
         })
+        .catch(err => {
+            res.status(500).json({
+                message: "Unable to get car list",
+                error: err
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.getById(id)
+        .then(car => {
+            res.status(200).json(car)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Unable to get that car",
+                error: err
+            })
+        })
+})
+
+router.post('/', (req, res) => {
+    const car = req.body;
+    db.insert(car)
+    .then(car => {
+        res.status(200).json(car)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Unable to add car to database",
+            error: err
+        })
+    })
 })
 
 module.exports = router;
